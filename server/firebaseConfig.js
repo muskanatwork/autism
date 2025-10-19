7b78e6895ae4e1d039ec4d8349a5f052c880af87
muskanatwork <kumari.muskan2302@gmail.com>
2025-10-19 16:30:53 +0530

    Final code fix: Use env var for Firebase and remove old imports

--- server/firebaseConfig.js
+++ server/firebaseConfig.js
@@ -1,8 +1,7 @@
 import admin from 'firebase-admin';
-import { createRequire } from 'module';
 
-const require = createRequire(import.meta.url);
-const serviceAccount = require('./serviceAccountKey.json');
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS); 
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
